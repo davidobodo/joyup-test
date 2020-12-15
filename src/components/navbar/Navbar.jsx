2f5759a68b97d1a0c6ac7f7af8020ad8a8634b34
David Obodo <obododavid5@gmail.com>
2020-12-15 16:57:10 +0100

    style: dropdown header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
+import "./Navbar.scss";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar__left-column">
-                <div className="dropdown">Location</div>
+                <div className="dropdown">
+                    <div className="dropdown__header">
+                        Location
+                        <div className="arrow"></div>
+                    </div>
+                    <ul className="dropdown__options">
+                        <li>Antonios pizza and burger</li>
+                        <li>Bamboo Asia </li>
+                        <li>Bamboo Asia</li>
+                        <li>Bamboo Asia</li>
+                        <li>Choice Market</li>
+                        <li>Cold Stone</li>
+                    </ul>
+                </div>
             </div>
             <div className="navbar__right-column">
                 <button className="btn btn-white">Menu Preview</button>
