ca14a6ee4d57805c304fb863578415ee467bb711
David Obodo <obododavid5@gmail.com>
2020-12-15 22:39:43 +0100

    feature; add calendar component

--- src/components/dateDisplay/DateDisplay.jsx
+++ src/components/dateDisplay/DateDisplay.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
+import { LinkedCalendar } from "rb-datepicker";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-daterangepicker/daterangepicker.css";
 
 import "./DateDisplay.scss";
 const DateDisplay = ({ isDropdownOpen, handleToggleDropdown }) => {
-    console.log(isDropdownOpen, "DHSJDHJ");
+    const start = moment();
+    const end = moment().add(29, "days");
+    const [dateRange, setDateRange] = useState(start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY"));
+
+    //------------------------------------------------------------------
+    //When date changes fire this function
+    //------------------------------------------------------------------
+    const onDatesChange = ({ startDate, endDate }) => {
+        setDateRange(startDate.format("MMMM D, YYYY") + " - " + endDate.format("MMMM D, YYYY"));
+    };
+
+    const ranges = {
+        Today: [moment(), moment()],
+        Yesterday: [moment().subtract(1, "days"), moment().subtract(1, "days")],
+        "Last 7 Days": [moment().subtract(6, "days"), moment()],
+        "Last 30 Days": [moment().subtract(29, "days"), moment()],
+        "This Month": [moment().startOf("month"), moment().endOf("month")],
+        "Last Month": [moment().subtract(1, "month").startOf("month"), moment().subtract(1, "month").endOf("month")]
+    };
     return (
         <div className={isDropdownOpen ? "date-display-component is-open" : "date-display-component"}>
             <h4 className="date-display-component__header" onClick={handleToggleDropdown}>
-                Decembar 3, 2020 - December 9, 2020
+                {dateRange}
             </h4>
             <ul className="date-display-component__options">
                 <li>Today</li>
@@ -17,6 +39,27 @@ const DateDisplay = ({ isDropdownOpen, handleToggleDropdown }) => {
                 <li>Last Month</li>
                 <li>Custom Range</li>
             </ul>
+            <LinkedCalendar
+                onDatesChange={onDatesChange}
+                showDropdowns={false}
+                // startDate={start}
+                // endDate={end}
+                minDate={start}
+                showWeekNumbers={false}
+                showCustomRangeLabel={true}
+                range={{
+                    Today: [moment(), moment()],
+                    Yesterday: [moment().subtract(1, "days"), moment().subtract(1, "days")],
+                    "Last 7 Days": [moment().subtract(6, "days"), moment()],
+                    "Last 30 Days": [moment().subtract(29, "days"), moment()],
+                    "This Month": [moment().startOf("month"), moment().endOf("month")],
+                    "Last Month": [
+                        moment().subtract(1, "month").startOf("month"),
+                        moment().subtract(1, "month").endOf("month")
+                    ]
+                }}
+                alwaysShowCalendars={true}
+            />
         </div>
     );
 };
