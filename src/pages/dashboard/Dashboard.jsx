47b6cbbc4b45b1b773080345752d77bb4bf85cdc
David Obodo <obododavid5@gmail.com>
2020-12-15 14:44:10 +0100

    feature: complete styling and layout for cards section

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 
+import fbIcon from "../../assets/facebook.svg";
+import mesIcon from "../../assets/messenger.svg";
+import phIcon from "../../assets/phone.svg";
+import moneyIcon from "../../assets/money.svg";
+import infoIcon from "../../assets/info.svg";
+
 import "./Dashboard.scss";
 const DashboardPage = () => {
+    const CARD_INFO = useRef([
+        {
+            title: "Total Facebook Fans",
+            number: "200",
+            logo: fbIcon,
+            id: 1
+        },
+        {
+            title: "Total Messenger Opt-in Users",
+            number: "230",
+            logo: mesIcon,
+            id: 2
+        },
+        {
+            title: "Total Orders",
+            number: "320",
+            logo: phIcon,
+            id: 3
+        },
+        {
+            title: "Total Net Sales",
+            number: "$812.14",
+            logo: moneyIcon,
+            id: 4
+        },
+        {
+            title: "Average Order Value (%)",
+            number: "278.42",
+            logo: moneyIcon,
+            id: 5
+        }
+    ]).current;
     return (
         <div id="dashboard-page">
             <div className="dashboard-page">
@@ -15,7 +53,23 @@ const DashboardPage = () => {
                         <section className="dashboard-details__date-wrapper">
                             <h4>Decembar 3, 2020 - December 9, 2020</h4>
                         </section>
-                        <section className="dashboard-details__cards-wrapper"></section>
+                        <section className="dashboard-details__cards-wrapper">
+                            {CARD_INFO.map((item) => {
+                                const { title, number, logo, id } = item;
+                                return (
+                                    <div className="card" key={id}>
+                                        <h4 className="card__title">{title}</h4>
+                                        <div className="card__details">
+                                            <h1>{number}</h1>
+                                            <div className="logo-wrapper">
+                                                <img src={logo} alt="" />
+                                            </div>
+                                        </div>
+                                        <img src={infoIcon} alt="" className="card__info-icon" />
+                                    </div>
+                                );
+                            })}
+                        </section>
                         <section className="dashboard-details__graphs-wrapper"></section>
                     </div>
                 </main>
