4a9eacb041bf5762192402f917e34bd7ab5a7267
David Obodo <obododavid5@gmail.com>
2020-12-15 13:21:58 +0100

    feature: complete layout and styles of all components on  navbar except dropdown

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Navbar = () => {
-    return <nav className="navbar"></nav>;
+    return (
+        <nav className="navbar">
+            <div className="navbar__left-column">
+                <div className="dropdown">Location</div>
+            </div>
+            <div className="navbar__right-column">
+                <button className="btn btn-white">Menu Preview</button>
+                <button className="btn btn-yellow">Logout</button>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
