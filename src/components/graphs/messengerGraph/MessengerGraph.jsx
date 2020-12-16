cfb045a33b3f1501bd23b8701dc53efd6583c2c5
David Obodo <obododavid5@gmail.com>
2020-12-16 09:01:20 +0100

    feature: create messenger state for one day

--- src/components/graphs/messengerGraph/MessengerGraph.jsx
+++ src/components/graphs/messengerGraph/MessengerGraph.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
 
 import "./MessengerGraph.scss";
 
 const MessengerGraph = () => {
+    const [chartOptions, setChartOptions] = useState({
+        chart: {
+            id: "apexchart-example"
+        },
+        xaxis: {
+            categories: ["", "Today", , , , ,]
+        },
+        yaxis: {
+            min: 230,
+            max: 330
+        }
+    });
+
+    const [chartSeries, setChartSeries] = useState([
+        {
+            name: "series-1",
+            data: ["", 270, , , , ,]
+        }
+    ]);
     return (
         <div className="messenger-graph">
             <h4 className="messenger-graph__title">Active Messenger Subscribers</h4>
+            <div className="messenger-graph__content">
+                <Chart options={chartOptions} series={chartSeries} type="bar" width={500} height={320} />
+            </div>
         </div>
     );
 };
